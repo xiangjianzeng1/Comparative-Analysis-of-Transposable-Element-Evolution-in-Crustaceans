7920566e77274311938c39a3f39b1d150eb70af1
xiangjianzeng1 <zzzzzzzzxj@outlook.com>
2025-04-18 15:54:51 +0800

    Update step1.run_edta.sh

--- step1.run_edta.sh
+++ step1.run_edta.sh
@@ -1,7 +1,7 @@
 # source activate EDTA
 ln -s ../../12.ref/genome.fa genome.fa
 
-# 使用EDTA注释
+# Annotation with EDTA
 nohup EDTA.pl --genome genome.fa --species others --sensitive 1 --anno 1 --threads 30 --curatedlib ../../11.software/SINE_LINE.fa & 
 
 # source deactivate
